16cac2733915d29086f62cc44de2c9c099bdf8de
Maia Kikvadze <mako.kikvadze@gmail.com>
2023-02-01 19:18:12 +0400

    homework

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -15,13 +15,7 @@ public class test {
                         .get("http://ergast.com/api/f1/2017/circuits.json")
                         .then()
                         .extract().path("MRData.CircuitTable.Circuits.circuitId[1]");
-//        String firstCountry =
-//                given()
-//                        .pathParam("circuitId", firstCircuitId)
-//                        .when()
-//                        .get("http://ergast.com/api/f1/circuits/{circuitId}.json")
-//                        .then()
-//                        .extract().path("MRData.CircuitTable.Circuits.Location[0].country");
+
         String fifthCircuitId =
                 given()
                         .when()
@@ -29,17 +23,9 @@ public class test {
                         .then()
                         .extract().path("MRData.CircuitTable.Circuits.circuitId[5]");
 
-//        String fifthCountry =
-//                given()
-//                        .pathParam("circuitId", fifthCircuitId)
-//                        .when()
-//                        .get("http://ergast.com/api/f1/circuits/{circuitId}.json")
-//                        .then()
-//                        .extract().path("MRData.CircuitTable.Circuits.Location[0].country");
         System.out.println(firstCircuitId);
         System.out.println(fifthCircuitId);
-//        System.out.println(firstCountry);
-//        System.out.println(fifthCountry);
+
 
 
         return new Object[][]{
