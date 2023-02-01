43e2bf49318136a6919b157222b4e2cca91bcbe0
Maia Kikvadze <mako.kikvadze@gmail.com>
2023-02-01 18:59:06 +0400

    homework

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,30 +2,49 @@
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalToObject;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 
 public class test {
     @DataProvider(name = "circuitIdsAnsCountries")
     public Object[][] circuitIdsAnsCountries() {
 
-        String firstcircuitId =
+        String firstCircuitId =
                 given()
                         .when()
                         .get("http://ergast.com/api/f1/2017/circuits.json")
                         .then()
                         .extract().path("MRData.CircuitTable.Circuits.circuitId[1]");
-        String fifthcircuitId =
+        String firstCountry =
+                given()
+                        .pathParam("circuitId", firstCircuitId)
+                        .when()
+                        .get("http://ergast.com/api/f1/circuits/{circuitId}.json")
+                        .then()
+                        .extract().path("MRData.CircuitTable.Circuits.Location[0].country");
+        String fifthCircuitId =
                 given()
                         .when()
                         .get("http://ergast.com/api/f1/2017/circuits.json")
                         .then()
                         .extract().path("MRData.CircuitTable.Circuits.circuitId[5]");
-        System.out.println(firstcircuitId);
-        System.out.println(fifthcircuitId);
+
+        String fifthCountry =
+                given()
+                        .pathParam("circuitId", fifthCircuitId)
+                        .when()
+                        .get("http://ergast.com/api/f1/circuits/{circuitId}.json")
+                        .then()
+                        .extract().path("MRData.CircuitTable.Circuits.Location[0].country");
+        System.out.println(firstCircuitId);
+        System.out.println(fifthCircuitId);
+        System.out.println(firstCountry);
+        System.out.println(fifthCountry);
+
 
         return new Object[][]{
-                {firstcircuitId, "USA"},
-                {fifthcircuitId, "Hungary"}
+                {firstCircuitId, firstCountry},
+                {fifthCircuitId, fifthCountry}
         };
     }
 
@@ -37,6 +56,6 @@ public class test {
                 get("http://ergast.com/api/f1/circuits/{circuitId}.json").
                 then().
                 assertThat().
-                body("MRData.CircuitTable.Circuits.Location[0].country", equalToObject(country));
+                body("MRData.CircuitTable.Circuits.Location[0].country", equalTo(country));
     }
 }
