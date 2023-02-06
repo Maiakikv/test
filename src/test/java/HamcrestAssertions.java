e1d3a81f81f7f5fc2ada58905a6000c443065a0b
Maia Kikvadze <mako.kikvadze@gmail.com>
2023-02-06 21:11:10 +0400

    homework

--- src/test/java/HamcrestAssertions.java
+++ src/test/java/HamcrestAssertions.java
@@ -1,13 +1,13 @@
 import io.restassured.response.Response;
+import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasValue;
-import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
-import static org.hamcrest.number.OrderingComparison.lessThanOrEqualTo;
+
 
 public class HamcrestAssertions {
     @Test
@@ -21,9 +21,12 @@ public class HamcrestAssertions {
                 then()
                 .assertThat()
                 .body("MRData.CircuitTable.Circuits.circuitId", hasItem("marina_bay"))
-                .body("MRData.CircuitTable.Circuits.Location[-1].long",
-                        anyOf(greaterThan("1"), equalTo("10")))
                 .body("MRData.CircuitTable.Circuits.Location[1, -1].country", hasItems("USA", "UAE"));
+
+        String locationL = response.then().extract().jsonPath().getString("MRData.CircuitTable.Circuits.Location[-1].long");
+        double locationLong = Double.parseDouble(locationL);
+        assertThat(locationLong, Matchers.anyOf(greaterThan(1.0),equalTo(10.0)));
+
 
 
     }
